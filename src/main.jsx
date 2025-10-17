9cd2df226f42e63dcd813abd9ca27b6804c7a303
Tricoci-Sheppard <csheppard@tricociuniversity.edu>
2025-10-16 19:30:29 -0500

    fixed

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SignatureBuilder from './SignatureBuilder.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SignatureBuilder />
+  </React.StrictMode>
+)
